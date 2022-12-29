578cd01970135942cca38da4aec1e4d5f3ffe0f7
Emir Taha Karcı <emirtahakarci12@gmail.com>
2022-12-29 16:55:56 +0300

    güncelleme

--- src/Wallet/UI/MainWindow.java
+++ src/Wallet/UI/MainWindow.java
@@ -1,11 +1,15 @@
 package Wallet.UI;
 
-import java.awt.Color;
-import java.awt.Toolkit;
-import javax.swing.*;
+
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
 
 public class MainWindow{
+
+    private static JPanel panel;
+    private static Toolkit arac;
 
     public static void screen(){
         Toolkit arac = Toolkit.getDefaultToolkit();
@@ -34,17 +38,49 @@ public class MainWindow{
 
 
 
+
+        String isim="Emir Taha";
+        JLabel labelHosgeldin = new JLabel("Merhaba "+isim);
+        labelHosgeldin.setFont(new Font("Merhaba", Font.PLAIN, 36));
+        labelHosgeldin.setForeground(new Color(50, 50, 10));
+        labelHosgeldin.setBounds(arac.getScreenSize().width/2/7*2, arac.getScreenSize().height/2/4, arac.getScreenSize().width/2/7*6, 100);
+        panel.add(labelHosgeldin);
+
+/*
+            BİLGİLERİM EKRANI
+        String data[][]  = { {"Adı","Ahmet"},
+                {"Soy adı","Hayri"},
+                {"TC","12345678987"},
+                {"ev adresi","hürriyet mah bakırcılar sok no19 daire 2"},
+                {"cep tel","05380300803"},
+                {"para durumu","1234"}};
+        String column[]={"Bilgiler","Icerik"};
+        JTable jtable=new JTable(data,column);
+
+        jtable.setBounds(arac.getScreenSize().width/2/7*2, arac.getScreenSize().height/2/10, arac.getScreenSize().width/2/7*4, arac.getScreenSize().height/2/4*2);
+        panel.add(jtable);
+        */
+        /*
+        int para=12345;
+        JLabel labelPara = new JLabel("Bakiyeniz "+para+"TL");
+        labelPara.setFont(new Font("Para", Font.PLAIN, 36));
+        labelPara.setForeground(new Color(50, 50, 10));
+        labelPara.setBounds(arac.getScreenSize().width/2/7*2, arac.getScreenSize().height/2/4, arac.getScreenSize().width/2/7*6, 100);
+        panel.add(labelPara);
+        */
+
+
         panel.add(profil);
         panel.add(hesabım);
         panel.add(eft);
         panel.add(odeme);
 
-        JLabel lblText = new JLabel("Hello World!", SwingConstants.CENTER);
-        pencere.getContentPane().add(lblText);
-
 
         pencere.add(panel);
         pencere.setVisible(true);
 
+
     }
+
+
 }
