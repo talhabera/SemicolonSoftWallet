71ede78c386d991b9fc8378e46285a6f528d08f6
Talha Bera <talha.bera@adayazilim.com>
2023-01-02 23:50:46 +0300

    Kodlar düzenlendi

--- src/Wallet/UI/Aktarim.java
+++ src/Wallet/UI/Aktarim.java
@@ -5,26 +5,25 @@ import javax.swing.*;
 import static Wallet.UI.MainWindow.*;
 
 public class Aktarim {
-    public static JTextField textBox1 = new JTextField();
-    public static JTextField textBox2 = new JTextField();
+    public static JTextField txtIban = new JTextField();
+    public static JTextField txtTutar = new JTextField();
+    public static JLabel lIban = new JLabel("Iban NUMARASI ");
+    public static JLabel lTutar = new JLabel("Göndermek İstediğiniz Tutar");
     public static JButton gonder = new JButton("Gönder");
-    public static JLabel iban = new JLabel("Iban NUMARASI ");
-    public static JLabel tutar = new JLabel("Göndermek İstediğiniz Tutar");
-
 
     public static void aktarim() {
         screen();
 
-        iban.setBounds(arac.getScreenSize().width / 2 / 7 * 3, arac.getScreenSize().height / 2 / 10, arac.getScreenSize().width / 2 / 3, arac.getScreenSize().height / 2 / 12);
-        textBox1.setBounds(arac.getScreenSize().width / 2 / 7 * 3, arac.getScreenSize().height / 2 / 10 * 2, arac.getScreenSize().width / 2 / 3, arac.getScreenSize().height / 2 / 12);
-        tutar.setBounds(arac.getScreenSize().width / 2 / 7 * 3, arac.getScreenSize().height / 2 / 10 * 3, arac.getScreenSize().width / 2 / 3, arac.getScreenSize().height / 2 / 12);
-        textBox2.setBounds(arac.getScreenSize().width / 2 / 7 * 3, arac.getScreenSize().height / 2 / 10 * 4, arac.getScreenSize().width / 2 / 3, arac.getScreenSize().height / 2 / 12);
+        lIban.setBounds(arac.getScreenSize().width / 2 / 7 * 3, arac.getScreenSize().height / 2 / 10, arac.getScreenSize().width / 2 / 3, arac.getScreenSize().height / 2 / 12);
+        txtIban.setBounds(arac.getScreenSize().width / 2 / 7 * 3, arac.getScreenSize().height / 2 / 10 * 2, arac.getScreenSize().width / 2 / 3, arac.getScreenSize().height / 2 / 12);
+        lTutar.setBounds(arac.getScreenSize().width / 2 / 7 * 3, arac.getScreenSize().height / 2 / 10 * 3, arac.getScreenSize().width / 2 / 3, arac.getScreenSize().height / 2 / 12);
+        txtTutar.setBounds(arac.getScreenSize().width / 2 / 7 * 3, arac.getScreenSize().height / 2 / 10 * 4, arac.getScreenSize().width / 2 / 3, arac.getScreenSize().height / 2 / 12);
         gonder.setBounds(arac.getScreenSize().width / 2 / 7 * 3, arac.getScreenSize().height / 2 / 10 * 5 + 10, arac.getScreenSize().width / 2 / 3, arac.getScreenSize().height / 2 / 12);
 
-        panel.add(iban);
-        panel.add(textBox1);
-        panel.add(textBox2);
-        panel.add(tutar);
+        panel.add(lIban);
+        panel.add(txtIban);
+        panel.add(txtTutar);
+        panel.add(lTutar);
         panel.add(gonder);
     }
 }
